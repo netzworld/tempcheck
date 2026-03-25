84811597315bdc938240443057926850ac5d4d5f
Nic <nicmrandazzo@gmail.com>
2026-03-25 12:29:03 -0700

    Updated device firmware, changed timings in setup and sendData

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,47 @@ Adafruit_BME280 bme;
 
 WiFiClient client;
 
-const char *server = "192.168.4.1"; // Flask server IP
+const char *server = "10.0.0.138"; // Pi's IP on home WiFi (run `hostname -I` on Pi)
 const int port = 5000;
 
 void connectWiFi()
 {
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  WiFi.setPins(8, 7, 4, 2); // Feather M0: CS, IRQ, RST, EN
 
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
+    Serial.println("WiFi module not found!");
+    while (true)
+      ;
+  }
+
+  Serial.print("Firmware version: ");
+  Serial.println(WiFi.firmwareVersion());
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.print("Connecting");
+
+  int attempts = 0;
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.print(".");
+    attempts++;
+
+    if (attempts >= 20)
+    { // 10 seconds
+      Serial.println("\nConnection timed out. Halting.");
+      while (true)
+        ; // halt so you can read serial output
+    }
   }
 
-  Serial.println("\nConnected to WiFi!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("\nConnected!");
+  Serial.print("IP: ");
+  IPAddress ip = WiFi.localIP();
+  Serial.print(ip[0]); Serial.print(".");
+  Serial.print(ip[1]); Serial.print(".");
+  Serial.print(ip[2]); Serial.print(".");
+  Serial.println(ip[3]);
 }
 
 void sendData(float temperature, float pressure, float humidity)
@@ -40,16 +64,16 @@ void sendData(float temperature, float pressure, float humidity)
     payload += "\"pressure\":" + String(pressure, 2);
     payload += "}";
 
-    String request = String("POST /log HTTP/1.1\r\n") +
+    String request = String("POST /api/data HTTP/1.1\r\n") +
                      "Host: " + server + "\r\n" +
                      "Content-Type: application/json\r\n" +
                      "Content-Length: " + payload.length() + "\r\n\r\n" +
                      payload;
 
-    client.print(request);
-    Serial.println("Sent data:");
-    Serial.println(payload);
-    delay(100);
+    // client.print(request);
+    // Serial.println("Sent data:");
+    // Serial.println(payload);
+    delay(1000);
     client.stop();
   }
   else
@@ -61,7 +85,7 @@ void sendData(float temperature, float pressure, float humidity)
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
+  delay(8000);
   connectWiFi();
 
   if (!bme.begin(0x76))
@@ -74,12 +98,12 @@ void setup()
 
 void loop()
 {
-  Serial.println(WiFi.localIP());
-  Serial.println(WiFi.gatewayIP());
+  // Serial.println(WiFi.localIP());
+  // Serial.println(WiFi.gatewayIP());
   float temp = (bme.readTemperature() * 9.0/5.0) + 32.0; 
   float hum = bme.readHumidity();
   float pres = bme.readPressure() / 100.0F;
 
-  sendData(temp, hum, pres);
-  delay(10000);
+    sendData(temp, pres, hum);
+  delay(4000);
 }
